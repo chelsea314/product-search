b36dd908486d9de209eb1e6cfc493f59c1e17524
chelsea314 <chelseansexton@gmail.com>
2023-09-07 13:59:58 -0500

    single product retrieved to console upon product selection

--- src/app/components/option.js
+++ src/app/components/option.js
@@ -1,12 +1,14 @@
 export default function Option({ product }) {
+
+  function handleSelection(e) {
+    fetch('https://dummyjson.com/products/'+ e.target.value)
+      .then(res => res.json())
+      .then(console.log);
+  }
+
   return (
-    <>
-      <option
-        value={product.id}
-        key={product.id}
-      >
-        {product.title}
-      </option>
-    </>
+    <option value={product.id} key={product.id} onClick={handleSelection}>
+      {product.title}
+    </option>
   );
 }
