1927fa58d11b83dcef5d86efad409687038460cb
chelsea314 <chelseansexton@gmail.com>
2023-09-12 15:53:22 -0500

    inter font applied

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  weight: ['400', '700'] 
+})
 
 export const metadata = {
   title: 'Shop til You Drop',
   description: 'Front End Take Home Assessment by Chelsea',
+
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body
       className={inter.className}>{children}</body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import styles from "./page.module.css";
 import Dropdown from "./components/Dropdown"
