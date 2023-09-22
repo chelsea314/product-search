cc9f648b3a7552bb8d1892b878461616255a0d59
chelsea314 <chelseansexton@gmail.com>
2023-09-22 14:05:06 -0500

    removed tailwind. Set positioning for product data on larger screen sizes

--- src/app/components/Practice.js
+++
@@ -1,7 +0,0 @@
-export default function Practice() {
-    return (
-      <div className="container mx-auto">
-        <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-      </div>
-    )
-  }
